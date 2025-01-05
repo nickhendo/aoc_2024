1962581268e67d1a5ccefe6e9b7c3dc1ba226f4c
Nick Henderson <nhenderson019@gmail.com>
2025-01-05 16:00:48 +1000

    Save progress

--- day9/src/part2.zig
+++ day9/src/part2.zig
@@ -5,6 +5,147 @@ const expect = std.testing.expect;
 const assert = std.debug.assert;
 const print = std.debug.print;
 
+fn defragment(disk_array: *[]i16, puzzle_input: []const u8, heaps: *[10]heap.MinHeap(usize)) !void {
+    var index = puzzle_input.len;
+
+    var j: i16 = @intCast(puzzle_input.len / 2);
+    while (index > 0) {
+        index -= 1;
+
+        if (@mod(index, 2) == 1) {
+            continue;
+        }
+
+        var remaining_space_size: usize = 0;
+        var remaining_space_index: usize = 0;
+
+        const block_size = getNextHeap(heaps, puzzle_input[index]);
+
+        if (block_size == null or block_size == 0) {
+            if (j > 0) {
+                j -= 1;
+            }
+            continue;
+        }
+        var value = &heaps[block_size.?];
+
+        const disk_buffer_index = getIndex(index, puzzle_input);
+
+        if (value.head().? > disk_buffer_index) {
+            continue;
+        }
+
+        const block = try value.*.delete();
+
+        if (block == null) {
+            unreachable;
+        }
+
+        remaining_space_size = block_size.? - puzzle_input[index];
+        remaining_space_index = block.? + puzzle_input[index];
+        try heaps[remaining_space_size].insert(remaining_space_index);
+
+        for (disk_array, 0..) |*ivalue, iii| {
+            if (iii < block.? or iii >= block.? + puzzle_input[index]) {
+                continue;
+            }
+            ivalue.* = j;
+        }
+
+        for (disk_array[disk_buffer_index .. disk_buffer_index + puzzle_input[index]]) |*val| {
+            val.* = -1;
+        }
+
+        if (j > 0) {
+            j -= 1;
+        }
+    }
+}
+
+test defragment {
+    const TestCase = struct {
+        disk_array: []i16,
+        puzzle_input: []const u8,
+        size: usize,
+        expected: []const i16,
+    };
+
+    const test_cases = [_]TestCase{
+        .{
+            .disk_array = &[_]i16{ 0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2 },
+            .puzzle_input = &[_]u8{ 1, 2, 3, 4, 5 },
+            .size = 15,
+            .expected = &[_]i16{ 0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2 },
+        },
+        // .{
+        //     .input = &[_]u8{ 1, 0, 1, 0, 1 },
+        //     .size = 3,
+        //     .expected = &[_]i16{ 0, 1, 2 },
+        // },
+        // .{
+        //     .input = &[_]u8{ 2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2, 9, 1, 2, 3 },
+        //     .size = 57,
+        //     .expected = &[_]i16{ 0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, -1, -1, 11, 11, 11 },
+        // },
+    };
+
+    for (test_cases) |test_case| {
+        var heaps: [10]heap.MinHeap(usize) = undefined;
+        for (&heaps) |*value| {
+            value.* = heap.MinHeap(usize).init(std.testing.allocator);
+        }
+
+        defer {
+            for (&heaps) |*value| {
+                value.*.deinit();
+            }
+        }
+
+        try defragment(&test_case.disk_array, test_case.puzzle_input, &heaps);
+        try std.testing.expectEqualSlices(i16, test_case.expected, test_case.disk_array);
+    }
+}
+
+pub fn getResult(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    // Convert puzzle input to an integer array that is easier to work with
+    var input_buffer = try allocator.alloc(u8, input.len);
+    defer allocator.free(input_buffer);
+    const size_of_disk = convertInputToArray(&input_buffer, input);
+
+    // Setup array of heaps for tracking empty blocks
+    var heaps: [10]heap.MinHeap(usize) = undefined;
+    for (&heaps) |*value| {
+        value.* = heap.MinHeap(usize).init(allocator);
+    }
+    defer {
+        for (&heaps) |*value| {
+            value.*.deinit();
+        }
+    }
+
+    // Set up disk with array of blocks and fragments
+    var disk = Disk{
+        .array = try allocator.alloc(i16, size_of_disk),
+    };
+    defer allocator.free(disk.array);
+
+    try populateDiskData(&disk, input_buffer, &heaps);
+
+    try defragment(&disk.array, input_buffer, &heaps);
+
+    return calculateChecksum(disk.array);
+}
+
+test getResult {
+    var test_input: []const u8 = "2333133121414131402";
+    var answer = try getResult(std.testing.allocator, test_input);
+    try std.testing.expectEqual(2858, answer);
+
+    test_input = "23331331214141314029123";
+    answer = try getResult(std.testing.allocator, test_input);
+    try std.testing.expectEqual(3356, answer);
+}
+
 const Disk = struct {
     array: []i16,
 
@@ -59,10 +200,7 @@ fn populateDiskData(disk: *Disk, input: []const u8, heaps: *[10]heap.MinHeap(usi
 
     var block_id: i16 = 0;
 
-    for (input, 0..) |char, block_index| {
-        // Block size is the characters integer value
-        const block_size = char - 48;
-
+    for (input, 0..) |block_size, block_index| {
         // Odd block indexes indicate gaps in the disk, i.e. '...'
         if (@mod(block_index, 2) == 1) {
             try heaps[block_size].insert(disk_index);
@@ -87,12 +225,58 @@ fn populateDiskData(disk: *Disk, input: []const u8, heaps: *[10]heap.MinHeap(usi
     }
 }
 
-test populateDiskData {}
+test populateDiskData {
+    const allocator = std.testing.allocator;
 
-fn calculateChecksum(disk: Disk) i64 {
+    const TestCase = struct {
+        input: []const u8,
+        size: usize,
+        expected: []const i16,
+    };
+    const test_cases = [_]TestCase{
+        .{
+            .input = &[_]u8{ 1, 2, 3, 4, 5 },
+            .size = 15,
+            .expected = &[_]i16{ 0, -1, -1, 1, 1, 1, -1, -1, -1, -1, 2, 2, 2, 2, 2 },
+        },
+        .{
+            .input = &[_]u8{ 1, 0, 1, 0, 1 },
+            .size = 3,
+            .expected = &[_]i16{ 0, 1, 2 },
+        },
+        .{
+            .input = &[_]u8{ 2, 3, 3, 3, 1, 3, 3, 1, 2, 1, 4, 1, 4, 1, 3, 1, 4, 0, 2, 9, 1, 2, 3 },
+            .size = 57,
+            .expected = &[_]i16{ 0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9, -1, -1, -1, -1, -1, -1, -1, -1, -1, 10, -1, -1, 11, 11, 11 },
+        },
+    };
+
+    for (test_cases) |test_case| {
+        var disk = Disk{
+            .array = try allocator.alloc(i16, test_case.size),
+        };
+        defer allocator.free(disk.array);
+
+        var heaps: [10]heap.MinHeap(usize) = undefined;
+        for (&heaps) |*value| {
+            value.* = heap.MinHeap(usize).init(std.testing.allocator);
+        }
+
+        defer {
+            for (&heaps) |*value| {
+                value.*.deinit();
+            }
+        }
+
+        try populateDiskData(&disk, test_case.input, &heaps);
+        try std.testing.expectEqualSlices(i16, test_case.expected, disk.array);
+    }
+}
+
+fn calculateChecksum(array: []const i16) i64 {
     var checksum: i64 = 0;
 
-    for (disk.array, 0..) |val, index| {
+    for (array, 0..) |val, index| {
         if (val == -1) {
             continue;
         }
@@ -103,96 +287,10 @@ fn calculateChecksum(disk: Disk) i64 {
     return checksum;
 }
 
-test calculateChecksum {}
-
-pub fn getResult(allocator: std.mem.Allocator, input: []const u8) !i64 {
-    var input_buffer = try allocator.alloc(u8, input.len);
-    defer allocator.free(input_buffer);
-
-    const size_of_disk = convertInputToArray(&input_buffer, input);
-
-    var heaps: [10]heap.MinHeap(usize) = undefined;
-    for (&heaps) |*value| {
-        value.* = heap.MinHeap(usize).init(allocator);
-    }
-
-    defer {
-        for (&heaps) |*value| {
-            value.*.deinit();
-        }
-    }
-
-    var disk = Disk{
-        .array = try allocator.alloc(i16, size_of_disk),
-    };
-    defer allocator.free(disk.array);
-
-    try populateDiskData(&disk, input, &heaps);
-
-    var index = input_buffer.len;
-    var j: i16 = @intCast(input_buffer.len / 2);
-    while (index > 0) {
-        index -= 1;
-
-        if (@mod(index, 2) == 0) {
-            var remaining_space_size: usize = 0;
-            var remaining_space_index: usize = 0;
-
-            const block_size = getNextHeap(&heaps, input_buffer[index]);
-
-            if (block_size == null or block_size == 0) {
-                if (j > 0) {
-                    j -= 1;
-                }
-                continue;
-            }
-            var value = &heaps[block_size.?];
-
-            const disk_buffer_index = getIndex(index, input_buffer);
-
-            if (value.head().? > disk_buffer_index) {
-                continue;
-            }
-
-            const block = try value.*.delete();
-
-            if (block == null) {
-                unreachable;
-            }
-
-            remaining_space_size = block_size.? - input_buffer[index];
-            remaining_space_index = block.? + input_buffer[index];
-            try heaps[remaining_space_size].insert(remaining_space_index);
-
-            for (disk.array, 0..) |*ivalue, iii| {
-                if (iii < block.? or iii >= block.? + input_buffer[index]) {
-                    continue;
-                }
-                ivalue.* = j;
-            }
-
-            for (disk.array[disk_buffer_index .. disk_buffer_index + input_buffer[index]]) |*val| {
-                val.* = -1;
-            }
-
-            if (j > 0) {
-                j -= 1;
-            }
-        }
-    }
-
-    const checksum = calculateChecksum(disk);
-    return checksum;
-}
-
-test getResult {
-    var test_input: []const u8 = "2333133121414131402";
-    var answer = try getResult(std.testing.allocator, test_input);
-    try std.testing.expectEqual(2858, answer);
-
-    test_input = "23331331214141314029123";
-    answer = try getResult(std.testing.allocator, test_input);
-    try std.testing.expectEqual(3356, answer);
+test calculateChecksum {
+    const array = [_]i16{ 0, 1, 0, -1, 2, 3, 4, 5, 8, 100 };
+    const answer = calculateChecksum(&array);
+    try std.testing.expectEqual(1047, answer);
 }
 
 /// Iterate through array of heaps. Of the heaps with a suitable head,
